dacf01c8bdca76c97fd8908137bead9b0591b0f5
DenisSychev95 <359_08@mail.ru>
2025-04-04 11:18:42 +0300

    added Domashka22.py

---
+++ Domashka22.py
@@ -0,0 +1,39 @@
+class Converter:
+    def __init__(self, weight=0):
+        self.__weight = weight
+
+    @staticmethod
+    def __check_value(value):
+        if isinstance(value, int) or isinstance(value, float):
+            return True
+        else:
+            return False
+
+    @property
+    def weight(self):
+        return f"{self.__weight} кг => {round((self.convert()), 3)} фунтов"
+
+    @weight.setter
+    def weight(self, weight):
+        if Converter.__check_value(weight):
+            self.__weight = weight
+        else:
+            print("Килограммы задаются только числами")
+
+    @weight.deleter
+    def weight(self):
+        del self.__weight
+
+    def convert(self):
+        res = self.__weight * 2.205
+        return res
+
+
+p1 = Converter()
+# print(p1.weight)
+p1.weight = 12
+print(p1.weight)
+p1.weight = 41
+print(p1.weight)
+p1.weight = "двенадцать"
+# del p1.weight
