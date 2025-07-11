9678b95934bd9aadfbd97f2fb2b0d87c5e4c5956
DenisSychev95 <359_08@mail.ru>
2025-07-11 11:30:03 +0300

    11/07/25 update1

--- JS/Domashka_JS_1/script.js
+++ JS/Domashka_JS_1/script.js
@@ -5,6 +5,7 @@ let s_r2 = "Скидка: ";
 let s_r3 = "Итоговая стоимость: ";
 let currency = "руб";
 let discount = "отсутствует";
+let s_null = "неизвестна"
 let discount1 = 5;
 let discount2 = 3;
 let summ = prompt("Введите стоимость покупки: ");
@@ -51,7 +52,8 @@ let row2, row3;
 //     alert(row1 + "\n" + row2 + "\n"+row3);
 // };
 
+let res0 = s_r1 + s_null + "\n" + s_r2 + discount + "\n" + s_r3 + s_null;
 let res1 = row1 + "\n"+ s_r2 + discount1 + "%" +"\n" + s_r3 + (0.01*(100-discount1)*summ).toFixed(2) + currency;
 let res2 = row1 + "\n" + s_r2 + discount2 + "%" + "\n" + s_r3 + (0.01*(100-discount2)*summ).toFixed(2) + currency;
 let res3 = row1 + "\n" + s_r2 + discount + "\n" + s_r3 + summ + currency;
-summ <= 500 ? alert(res3) : summ < 1001 ? alert(res2) : alert(res1);
+summ == null ? alert(res0) :summ <= 500 ? alert(res3) : summ < 1001 ? alert(res2) : alert(res1);
